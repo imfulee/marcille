aa11c74bfddccf8ef1292ff2c9caebbf461c0d8c
imfulee <freems5192@gmail.com>
2024-07-23 15:13:50 +0800

    chore: formatting

--- arch_post_install.sh
+++ arch_post_install.sh
@@ -4,124 +4,124 @@ os_name=$(grep -E "^(NAME)=" /etc/os-release | awk -F = '{ print $2 }' | sed 's/
 documents_dir=$HOME/Documents
 
 if ! command -v pacman &>/dev/null; then
-	is_arch=true
+  is_arch=true
 fi
 
 if [ "$os_name" == "EndeavourOS" ]; then
-	is_endeavouros=true
+  is_endeavouros=true
 fi
 
-function join {
-	local IFS="$1"
-	shift
-	echo "$*"
+join() {
+  local IFS="$1"
+  shift
+  echo "$*"
 }
 
 if [[ ! -d $documents_dir ]]; then
-	documents_dir=$HOME/documents
-	mkdir -p $documents_dir
+  documents_dir=$HOME/documents
+  mkdir -p $documents_dir
 fi
 
 arch_packages=(
-	# package managers
-	"flatpak"
-	# input methods
-	"fcitx5-im"
-	"fcitx5-chinese-addons"
-	# containers
-	"docker"
-	"docker-compose"
-	"podman"
-	"podman-compose"
-	# wayland
-	"wl-clipboard"
-	# tools
-	"alacritty"
-	"btop"
-	"chezmoi"
-	"cmake"
-	"curlie"
-	"diff-so-fancy"
-	"fd"
-	"fwupd"
-	"fzf"
-	"gcc"
-	"git"
-	"jq"
-	"k9s"
-	"lazygit"
-	"libreoffice-fresh"
-	"make"
-	"neovim"
-	"net-tools"
-	"pandoc"
-	"pyenv"
-	"ripgrep"
-	"speedtest-cli"
-	"stow"
-	"tree"
-	"unzip"
-	"wget"
-	"zellij"
-	"zip"
-	"zsh"
-	# programming
-	"go"
-	# gui applications
-	"bitwarden"
-	"gparted"
-	"gthumb"
-	"gnome-boxes"
-	"thunderbird"
-	"transmission-qt"
-	"virtualbox"
-	"virtualbox-guest-iso"
-	# fonts
-	"nerd-fonts"
-	"noto-fonts"
-	"noto-fonts-cjk"
-	"noto-fonts-emoji"
-	"noto-fonts-extra"
-	# system
-	"linux-zen"
+  # package managers
+  "flatpak"
+  # input methods
+  "fcitx5-im"
+  "fcitx5-chinese-addons"
+  # containers
+  "docker"
+  "docker-compose"
+  "podman"
+  "podman-compose"
+  # wayland
+  "wl-clipboard"
+  # tools
+  "alacritty"
+  "btop"
+  "chezmoi"
+  "cmake"
+  "curlie"
+  "diff-so-fancy"
+  "fd"
+  "fwupd"
+  "fzf"
+  "gcc"
+  "git"
+  "jq"
+  "k9s"
+  "lazygit"
+  "libreoffice-fresh"
+  "make"
+  "neovim"
+  "net-tools"
+  "pandoc"
+  "pyenv"
+  "ripgrep"
+  "speedtest-cli"
+  "stow"
+  "tree"
+  "unzip"
+  "wget"
+  "zellij"
+  "zip"
+  "zsh"
+  # programming
+  "go"
+  # gui applications
+  "bitwarden"
+  "gparted"
+  "gthumb"
+  "gnome-boxes"
+  "thunderbird"
+  "transmission-qt"
+  "virtualbox"
+  "virtualbox-guest-iso"
+  # fonts
+  "nerd-fonts"
+  "noto-fonts"
+  "noto-fonts-cjk"
+  "noto-fonts-emoji"
+  "noto-fonts-extra"
 )
 
 if is_arch; then
-	if is_endeavouros; then
-		arch_packages+=(
-			# package manager
-			"yay"
-		)
-	else
-		sudo pacman -S --needed git base-devel
-		git clone https://aur.archlinux.org/yay.git
-		cd yay
-		makepkg -si
-		cd $HOME
-		rm -rf yay
-	fi
+  if is_endeavouros; then
+    arch_packages+=(
+      # package manager
+      "yay"
+    )
+  else
+    sudo pacman -S --needed git base-devel
+    git clone https://aur.archlinux.org/yay.git
+    cd yay
+    makepkg -si
+    cd $HOME
+    rm -rf yay
+  fi
 else
-	# if not arch then exit
-	exit 0
+  # if not arch then exit
+  exit 0
+fi
+
 fi
 
 aur_packages=(
-	"visual-studio-code-bin"
-	"fnm-bin"
-	"slack-desktop"
-	"brave-bin"
-	"mongodb-compass"
-	"noto-fonts-sc"
-	"noto-fonts-tc"
-	"virtualbox-ext-oracle"
-	"zsh-antidote"
+  "visual-studio-code-bin"
+  "fnm-bin"
+  "slack-desktop"
+  "brave-bin"
+  "mongodb-compass"
+  "noto-fonts-sc"
+  "noto-fonts-tc"
+  "virtualbox-ext-oracle"
+  "zsh-antidote"
   "tmux-plugin-manager"
 )
 
 flatpak_packages=(
-	"com.spotify.Client"
-	"com.obsproject.Studio"
-	"io.dbeaver.DBeaverCommunity"
+  "com.spotify.Client"
+  "com.obsproject.Studio"
+  "io.dbeaver.DBeaverCommunity"
 )
 
 # install packages
