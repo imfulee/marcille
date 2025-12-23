0a5ce9e0fe50f11c15376a70815c942df910dd70
imfulee <freems5192@gmail.com>
2025-12-23 11:35:45 +0800

    feat: exec file

--- exec.sh
+++ exec.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 set -xeuo pipefail
 
-[[ -f arch_packages.sh ]] && bash arch_packages.sh || exit
-[[ -f base_setup.sh ]] && bash base_setup.sh
-[[ -f security.sh ]] && bash security.sh
-bash tool.sh
-bash steam.sh
+files=(
+    "arch_packages.sh"
+    "base_setup.sh"
+    "security.sh"
+    "tool.sh"
+    "steam.sh"
+)
+
+for file in "${files[@]}"; do
+    [[ -f "$file" ]] && bash "$file" || exit
+done
