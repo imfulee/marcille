4abb3a717c034bb4b372eac2b16c74e36e8b98ff
imfulee <freems5192@gmail.com>
2025-12-23 11:29:06 +0800

    feat: proton latest flag

--- steam.sh
+++ steam.sh
@@ -1,5 +1,5 @@
-source config.sh
-
+#!/bin/bash
+# shellcheck source=config.sh
 steam_compatibility_directory=$HOME/.steam/root/compatibilitytools.d
 
 get_proton_ge() {
@@ -9,14 +9,27 @@ get_proton_ge() {
   fi
 
   local proton_ge_version=$1
+  local url
+  local tarball_name
+  if [[ "$proton_ge_version" == "latest" ]]; then
+    url=$(
+      curl -s https://api.github.com/repos/GloriousEggroll/proton-ge-custom/releases/latest |
+        grep browser_download_url | cut -d\" -f4 |
+        grep .tar.gz
+    )
+  else
+    url="https://github.com/GloriousEggroll/proton-ge-custom/releases/download/"
+    url+="GE-Proton$proton_ge_version/GE-Proton$proton_ge_version.tar.gz"
+  fi
+  tarball_name=$(basename "$url")
 
-  echo "[download proton ge]"
+  echo "[download proton ge $tarball_name]"
   mkdir -p "$steam_compatibility_directory"
-  wget "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/GE-Proton$proton_ge_version/GE-Proton$proton_ge_version.tar.gz" \
+  wget "$url" \
     -P "$steam_compatibility_directory" \
     --no-verbose --show-progress
 
-  local tar_file=$steam_compatibility_directory/GE-Proton$proton_ge_version.tar.gz
+  local tar_file="$steam_compatibility_directory/$tarball_name"
 
   echo "[extract file]"
   pv "$tar_file" | tar xzf - -C "$steam_compatibility_directory"
@@ -34,6 +47,4 @@ rm_proton_ge() {
   [[ -d $to_delete_directory ]] && rm -rf "$to_delete_directory"
 }
 
-if $is_personal_machine; then
-  get_proton_ge 9-26
-fi
+get_proton_ge latest
